0b461e8f01f71f77c2e4e36a599aaa6476567e56
Abdurehman Saeed <asaeed526@gmail.com>
2022-11-29 17:51:53 +0300

    edited

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const server= http.createServer(app)
 const {Server}= require('socket.io')
 const io =new Server(server,{
     cors:{
-        origin:"http://localhost:3000"
+        origin:"https://incredible-dragon-5baa7d.netlify.app/"
     }
 })
 
