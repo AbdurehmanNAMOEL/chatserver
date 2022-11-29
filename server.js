84b31a4843c1591135f141046cd8200ebd2bea45
Abdurehman Saeed <asaeed526@gmail.com>
2022-11-29 17:38:24 +0300

    env added with port num

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const app = express()
 const http= require('http')
 const colors=require('colors')
 const cors= require('cors')
+require('dotenv').config()
 const server= http.createServer(app)
 const {Server}= require('socket.io')
-
 const io =new Server(server,{
     cors:{
         origin:"http://localhost:3000"
@@ -28,6 +28,6 @@ io.on('connection', socket=>{
 })
 
 app.use(cors())
-server.listen(3001,()=>{
+server.listen(process.env.PORT||5500,()=>{
     console.log('server listening on port 3001'.blue);
 })
